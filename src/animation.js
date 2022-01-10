2b49d826d53ca8837d3ab770385c3aec187b9ffd
Your Name <jazzysingh227@gmail.com>
2022-01-10 18:50:51 +0530

    animation

---
+++ src/animation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './animation.css'
+
+function Animation(props) {
+  return (
+    <div className='container'>
+      <div className='box'>
+        <h3>Loading...</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Animation;
